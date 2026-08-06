df3fe0a300ba086ad2301ab8e77ae21ddd8d4b87
Matheus Montovaneli <matheusmontovaneli2003@gmail.com>
2026-08-05 23:21:01 -0300

    feat: create show habit method

--- app/Http/Controllers/HabitController.php
+++ app/Http/Controllers/HabitController.php
@@ -6,22 +6,27 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreHabitRequest;
 use App\Http\Requests\UpdateHabitRequest;
+
 use App\Http\Resources\HabitResource;
+
 use App\Models\Habit;
-use App\Models\HabitLog;
 
 class HabitController extends Controller
 {
     public function index()
     {
+        request()->validate([
+            'with' => ['string', 'nullable', 'regex:/\b(?:logs|user)(?:.*\b(?:logs|user))?/i'],
+        ]);
+
         return HabitResource::collection(
             Habit::query()
                 ->when(
-                    str(request()->string('with', ''))->contains('user'),
+                    request()->string('with', '')->contains('user'),
                     fn ($query) => $query->with('user')
                 )
                 ->when(
-                    str(request()->string('with', ''))->contains('logs'),
+                    request()->string('with', '')->contains('logs'),
                     fn ($query) => $query->with('logs')
                 )
                 ->paginate()
@@ -30,30 +35,35 @@ class HabitController extends Controller
 
     public function show(Habit $habit)
     {
-        return HabitResource::make($habit);
+        request()->validate([
+            'with' => ['string', 'nullable', 'regex:/\b(?:logs|user)(?:.*\b(?:logs|user))?/i'],
+        ]);
+
+        $load = request()->string('with')->explode(',')->filter(fn ($w): bool => (string) $w !== '')->toArray();
+
+        return HabitResource::make(
+            $habit->load($load)
+        );
     }
 
     public function store(StoreHabitRequest $request)
     {
-        $data = $request->only(['title', 'uuid']);
-
-        $habit = Habit::create(array_merge($data, ['user_id' => 1]));
+        $habit = Habit::create(array_merge($request->validated(), ['user_id' => 1]));
 
         return HabitResource::make($habit);
     }
 
     public function update(UpdateHabitRequest $request, Habit $habit)
     {
-        $data = $request->validated();
-
-        $habit->update($data);
+        $habit->update($request->validated());
 
         return HabitResource::make($habit);
     }
 
     public function destroy(Habit $habit)
     {
-        HabitLog::whereHabitId($habit->id)->delete();
+        $habit->logs()->delete();
+
         $habit->delete();
 
         return response()->noContent();
