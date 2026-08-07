df8c751ad3d94857d54a1fbfe896a411f3ba99e0
Matheus Montovaneli <matheusmontovaneli2003@gmail.com>
2026-08-06 23:53:20 -0300

    feat: add habits models and refactor routes

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,5 @@ Route::prefix('api')->name('api.')->group(function (): void {
     Route::apiResource('habits.logs', HabitLogController::class)
         ->scoped(['habit' => 'uuid', 'log' => 'uuid'])
         ->only(['index', 'store', 'destroy', 'show']);
-
-    // Route::get('habits/{habit:uuid}/logs', [HabitLogController::class, 'index'])->name('habits.logs.index');
-    // Route::post('habits/{habit:uuid}/logs', [HabitLogController::class, 'store'])->name('habits.logs.store');
-    // Route::delete('habits/{habit:uuid}/logs/{log:uuid}', [HabitLogController::class, 'destroy'])->name('habits.logs.destroy');
+    
 });
